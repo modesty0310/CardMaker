6f733355afe78b2f98ba6cd63f943ba5e0065e23
modesty0310 <rlatprua4@gmail.com>
2020-12-14 14:31:41 +0900

    add firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import AuthService from '/service/auth_service'
+
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/service/auth_service.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+import firebaseApp from './firebase'
+
+class AuthService {
+  login(providerName) {
+    const authProvider = new firebase.auth.[`${providerName}AuthProvider`]();
+    return firebaseApp.auth().signInWithRedirect(authProvider);
+  }
+}
+
+export default AuthService;
