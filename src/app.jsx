7a694a7c0cbc8007bfea64adb5ef5441c1f940ad
modesty0310 <rlatprua4@gmail.com>
2020-12-15 10:37:26 +0900

    add login UI

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,20 @@
-import './app.css';
+import styles from './app.module.css';
 import Login from './components/login/login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App({authService}) {
-  return <Login authService={authService}/>;
+  return (
+    <div className={styles.app}>
+      <Login authService={authService}/>
+    </div>
+    
+  )
+  
 }
 
 export default App;
