2cbf11d2c9638dd6c558e6cd4d7b7cd6a2a20170
modesty0310 <rlatprua4@gmail.com>
2020-12-15 14:12:21 +0900

    add logout

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import styles from './app.module.css';
 import Login from './components/login/login';
+import Maker from './components/maker/maker';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,12 +8,22 @@ import {
   Link
 } from "react-router-dom";
 
+
 function App({authService}) {
   return (
     <div className={styles.app}>
-      <Login authService={authService}/>
+      <Router>
+        <Switch>
+          <Route path='/' exact>
+            <Login authService={authService}/>
+          </Route>
+          <Route path='/maker'>
+            <Maker authService={authService}/>
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
-    
   )
   
 }
